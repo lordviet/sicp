37204dd9db1a4dd48d458763c20a35010586a5b8
Dimitar Todev <dmtodev@gmail.com>
2022-01-09 03:11:11 +0200

    solve 1.35

--- chapter-1/1.35.rkt
+++ chapter-1/1.35.rkt
@@ -1,4 +1,19 @@
 #lang racket
 
-(define golden-ratio (/ (+ 1 (sqrt 5)) 2.0))
+(define tolerance 0.00001)
 
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+; The golden ratio by means of the fixed-point procedure
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+
+; Another way to compute, using a transformation 
+(fixed-point (lambda (x) (sqrt (+ x 1))) 1.0)
