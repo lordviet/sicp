c820e6a0f03968f0f74d6152aed026cd121a5bff
Dimitar Todev <35083347+lordviet@users.noreply.github.com>
2022-05-22 19:14:56 +0300

    add 1.21 and fermat test

---
+++ chapter-1/fermat-test.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(require racket/trace)
+
+(define (square x)
+  (* x x))
+
+; Computes the exponential of a number modulo another number
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
